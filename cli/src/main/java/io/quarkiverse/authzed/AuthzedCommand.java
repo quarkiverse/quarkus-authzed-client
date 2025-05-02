845318b5e080965ecb33180aa3c71e1552c986cd
Ioannis Canellos <iocanel@gmail.com>
2025-05-02 08:08:47 +0300

    feat: add --help option in the cli

--- cli/src/main/java/io/quarkiverse/authzed/AuthzedCommand.java
+++ cli/src/main/java/io/quarkiverse/authzed/AuthzedCommand.java
@@ -6,6 +6,7 @@ import io.quarkus.picocli.runtime.annotations.TopCommand;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Model.CommandSpec;
+import picocli.CommandLine.Option;
 import picocli.CommandLine.Spec;
 
 @TopCommand
@@ -15,6 +16,9 @@ public class AuthzedCommand implements Callable<Integer> {
 
     @Spec
     protected CommandSpec spec;
+
+    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Display this help message.")
+    public boolean help;
 
     @Override
     public Integer call() {
